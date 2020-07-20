0484e26a34bbedcd44ac35b04b5b180bad8b470f
Aaryamann Challani <aaryamannchallani7@gmail.com>
2020-07-20 17:27:01 +0530

    Fix with price :boom:

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const { table } = require("table");
 const ethers = require("ethers");
 require("dotenv").config();
+const { tokenmap, abi } = require("./tokendata");
+const { default: Axios } = require("axios");
+const api = 'https://api.coingecko.com/api/v3/coins/ethereum/contract/'
 
 let provider = ethers.getDefaultProvider();
-const { tokenmap, abi } = require("./tokendata");
 let contracts = new Map();
 let currencydata = new Map();
-let tabledata = [["Currency", "Symbol", "Supply"]];
+let tabledata = [["Currency", "Symbol", "Supply","Price"]];
 
 const makeContract = () => {
   for (let [key, value] of tokenmap) {
@@ -20,13 +22,14 @@ const getData = async () => {
       Name: await value.name(),
       Symbol: await value.symbol(),
       Supply: (await value.totalSupply()).toString(),
+      Price: '$'+(await Axios.get(api+tokenmap.get(key))).data['market_data']['current_price']['usd']
     });
   }
 };
 
 const makeTable = () => {
   for (let value of currencydata.values()) {
-    tabledata.push(Array(value["Name"], value["Symbol"], value["Supply"]));
+    tabledata.push(Array(value["Name"], value["Symbol"], value["Supply"],value["Price"]));
   }
 
   let output = table(tabledata);
