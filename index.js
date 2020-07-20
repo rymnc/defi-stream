e3b2040e64d08600a768ea4117190c9bf1112168
Aaryamann Challani <aaryamannchallani7@gmail.com>
2020-07-20 11:48:01 +0530

    Added Tokens

---
+++ index.js
@@ -0,0 +1,27 @@
+
+const ethers = require('ethers')
+require('dotenv').config()
+
+
+let provider = ethers.getDefaultProvider()
+const { tokenmap,abi } = require('./tokendata')
+let contracts = new Map()
+for(let [key,value] of tokenmap){
+    console.log(key+':'+value)
+    contracts.set(key,new ethers.Contract(value, abi, provider ))
+}
+
+let contractdata = new Map()
+const getData = async() =>{
+
+    for(let [key,value] of contracts){
+        contractdata.set(key, {
+            'Symbol': await value.symbol(),
+            "Total Supply": (await value.totalSupply()).toString()
+        })
+    }
+    
+}
+getData()
+
+
---
+++ tokendata.js
@@ -0,0 +1,12 @@
+let tokenmap = new Map() 
+    tokenmap.set("kyber",'0xdd974D5C2e2928deA5F71b9825b8b646686BD200')
+    tokenmap.set("synthetix",'0xC011a73ee8576Fb46F5E1c5751cA3B9Fe0af2a6F')
+    tokenmap.set("compound",'0xc00e94Cb662C3520282E6f5717214004A7f26888')
+
+
+
+module.exports = {
+    tokenmap,
+    abi : [{"constant":true,"inputs":[],"name":"name","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_spender","type":"address"},{"name":"_value","type":"uint256"}],"name":"approve","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"totalSupply","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_from","type":"address"},{"name":"_to","type":"address"},{"name":"_value","type":"uint256"}],"name":"transferFrom","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"decimals","outputs":[{"name":"","type":"uint8"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"_owner","type":"address"}],"name":"balanceOf","outputs":[{"name":"balance","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"symbol","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_to","type":"address"},{"name":"_value","type":"uint256"}],"name":"transfer","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"_owner","type":"address"},{"name":"_spender","type":"address"}],"name":"allowance","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"payable":true,"stateMutability":"payable","type":"fallback"},{"anonymous":false,"inputs":[{"indexed":true,"name":"owner","type":"address"},{"indexed":true,"name":"spender","type":"address"},{"indexed":false,"name":"value","type":"uint256"}],"name":"Approval","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"from","type":"address"},{"indexed":true,"name":"to","type":"address"},{"indexed":false,"name":"value","type":"uint256"}],"name":"Transfer","type":"event"}]
+
+}
