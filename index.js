10768e39815e5a1d12cf1ff218aadfe6b505ec5f
Aaryamann Challani <aaryamannchallani7@gmail.com>
2020-07-20 15:13:07 +0530

    Added polling

--- index.js
+++ index.js
@@ -1,48 +1,46 @@
-const { table } = require('table')
-const ethers = require('ethers')
-require('dotenv').config()
-const axios = require('axios')
-let defaultroute = 'https://api.coingecko.com/api/v3/simple/token_price/ethereum?contract_addresses='
+const { table } = require("table");
+const ethers = require("ethers");
+require("dotenv").config();
+const axios = require("axios");
+let defaultroute =
+  "https://api.coingecko.com/api/v3/simple/token_price/ethereum?contract_addresses=";
 
-
-let provider = ethers.getDefaultProvider()
-const { tokenmap,abi } = require('./tokendata')
-let contracts = new Map()
-for(let [key,value] of tokenmap){
-    console.log(key+':'+value)
-    contracts.set(key,new ethers.Contract(value, abi, provider ))
+let provider = ethers.getDefaultProvider();
+const { tokenmap, abi } = require("./tokendata");
+let contracts = new Map();
+for (let [key, value] of tokenmap) {
+  contracts.set(key, new ethers.Contract(value, abi, provider));
 }
 
-let contractdata = new Map()
-async function getData() { 
+let contractdata = new Map();
 
-    for(let [key,value] of contracts){
-        contractdata.set(key, {
-            'Symbol': await value.symbol(),
-            "Total Supply": (await value.totalSupply()).toString()
-        })
+function to_poll() {
+  let outputdata = [];
+  let data = [];
+  for (let key of contractdata.keys()) {
+    data.push(key);
+  }
+  outputdata.push(data);
+  async function hmm() {
+    let prices = [];
+    for (let [key, value] of tokenmap) {
+      let price = (await axios.get(defaultroute + value + "&vs_currencies=usd"))
+        .data[value.toLowerCase()].usd;
+      contractdata.set(key, {
+        Price: "$" + price,
+      });
+      prices.push("$" + price);
     }
-    console.log(contractdata)
-    
+    outputdata.push(prices);
+    try {
+      let output = table(outputdata);
+      console.log(output);
+    } catch {
+      console.log("Startup...");
+    }
+  }
+  hmm();
 }
-let outputdata=[]
-let data=[]
-getData().then(function(){
-    for(let key of contractdata.keys()){
-        data.push(key) 
-    }
-    outputdata.push(data)
-    let output = table(outputdata)
-    console.log(output)
-}).then(async function(){
-    for(let [key,value] of tokenmap){
-        contractdata.set(key, {
-            'Price': (await axios.get(defaultroute+value+'&vs_currencies=usd')).data[value.toLowerCase()].usd
-        })
-    }
-    console.log(contractdata)    
-})
-
-
-
-
+let poller = setInterval(() => {
+  to_poll();
+}, 10000);
