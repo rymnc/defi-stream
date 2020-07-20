37728a86c53452dc621ca37944bd93bab99b4ef4
Aaryamann Challani <aaryamannchallani7@gmail.com>
2020-07-20 16:42:59 +0530

    Prints table of data

--- index.js
+++ index.js
@@ -1,46 +1,46 @@
 const { table } = require("table");
 const ethers = require("ethers");
 require("dotenv").config();
-const axios = require("axios");
-let defaultroute =
-  "https://api.coingecko.com/api/v3/simple/token_price/ethereum?contract_addresses=";
 
 let provider = ethers.getDefaultProvider();
 const { tokenmap, abi } = require("./tokendata");
 let contracts = new Map();
-for (let [key, value] of tokenmap) {
-  contracts.set(key, new ethers.Contract(value, abi, provider));
+let currencydata = new Map();
+let tabledata = [['Currency','Symbol','Supply']]
+
+const makeContract = () => {
+  for (let [key, value] of tokenmap) {
+    contracts.set(key, new ethers.Contract(value, abi, provider));
+  }
+};
+
+const getData = async () => {
+  for (let [key, value] of contracts) {
+    currencydata.set(key, {
+      'Name': await value.name(),
+      'Symbol': await value.symbol(),
+      'Supply': (await value.totalSupply()).toString(),
+    });
+  }
+};
+
+const makeTable = () =>{
+    let names = []
+    let symbols = []
+    let supplies = []
+    for(let value of currencydata.values()){
+  
+        tabledata.push(Array(value['Name'],value['Symbol'],value['Supply']))
+    }
+
+    let output = table(tabledata)
+    console.log(output)
+    
 }
 
-let contractdata = new Map();
-
-function to_poll() {
-  let outputdata = [];
-  let data = [];
-  for (let key of contractdata.keys()) {
-    data.push(key);
-  }
-  outputdata.push(data);
-  async function updatePrice() {
-    let prices = [];
-    for (let [key, value] of tokenmap) {
-      let price = (await axios.get(defaultroute + value + "&vs_currencies=usd"))
-        .data[value.toLowerCase()].usd;
-      contractdata.set(key, {
-        Price: "$" + price,
-      });
-      prices.push("$" + price);
-    }
-    outputdata.push(prices);
-    try {
-      let output = table(outputdata);
-      console.log(output);
-    } catch {
-      console.log("Startup...");
-    }
-  }
-  updatePrice();
-}
-let poller = setInterval(() => {
-  to_poll();
-}, 10000);
+const main = async () => {
+  makeContract();
+  await getData();
+  makeTable()
+};
+main();
